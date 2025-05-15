6c0b57bf199deb907971f6281e87ba58d008ae5e
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2025-05-15 01:26:49 -0600

    feat: implement public access decorator and JWT authentication guard, update AuthController to use public access for register and login endpoints

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalPipes(new ValidationPipe());
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from '../../common/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
+    @Public()
     @Post('register')
     register(@Body() body: any) {
         return this.authService.register(body);
     }
 
+    @Public()
     @Post('login')
     login(@Body() body: { username: string; password: string }) {
         return this.authService.login(body.username, body.password);
